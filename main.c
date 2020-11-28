3e639c7d2bf01a522d434ff44d091b0d9ab1f0d7
DAVIDTS0286 <67129995+DAVIDTS0286@users.noreply.github.com>
2020-11-28 07:38:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ void encrypt();
 void decrypt();
 int fill();
 int delete();
+void intersection();
 
 int main(void)
 {
@@ -65,6 +66,7 @@ int main(void)
                 break;
 
             case 7:
+                intersection();
                 break;
 
             case 8:
@@ -243,4 +245,21 @@ int delete(){
     
 }
 
+void intersection(){
+
+        char resultado[27]="";
+        char palabra[50];
+        int i=0,j=0;
+        printf("Ingresa una frase!\n");scanf("%s",palabra);
+        while(palabra[i])
+        {
+            if(!strchr(resultado,palabra[i]))
+                resultado[j++]=palabra[i];
+            i++;
+        }
+        resultado[j]='\0';
+        printf("\nEl resultado seria!\n%s",resultado);
+
+        getchar();
+}
 
